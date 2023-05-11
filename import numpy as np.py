c217c4b4f8d4d2259df471183ad6268e59248fec
samikshat31 <133263269+samikshat31@users.noreply.github.com>
2023-05-12 03:00:07 +0530

    Update import numpy as np.py

--- import numpy as np.py
+++ import numpy as np.py
@@ -28,4 +28,4 @@ print("LAYER NORMALISATION")
 # perform mean normalization and variance scaling for each column
 for j in range(m.shape[0]):
     m__norm = gamma*((m[j] - col_means[j]) / np.sqrt(col_variances[j] + epsilon)) + beta
-    print(" Normalized column", j, ":", m__norm)
+    print(" Normalized row", j, ":", m__norm)
